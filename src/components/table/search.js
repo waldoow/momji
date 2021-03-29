959259d33853686cee200f421110b7d464cb17ea
waldow <alisahraoui.walid@gmail.com>
2021-03-29 22:28:51 +0200

    search bar filtering users by any field

--- src/components/table/search.js
+++
@@ -1,8 +0,0 @@
-import { Form } from 'react-bootstrap';
-
-const Search = () => {
-    return (
-        <Form.Control size="xl" type="text" placeholder="Search for an User here..." />
-    )
-}
-export default Search;
--- src/components/table/users-table.js
+++ src/components/table/users-table.js
@@ -1,40 +1,60 @@
-import { Table } from 'react-bootstrap';
+import { Table, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 const UsersTable = (props) => {
-    const users = props.users;
+    const users               = props.users;
+    const [filter, setFilter] = useState('');
     
+    const items  = Object.values(users).filter((user) => {
+        if ('' === filter) {
+            return user;
+        } else if(
+            user.profile.firstName.toLowerCase().includes(filter.toLowerCase()) ||
+            user.profile.lastName.toLowerCase().includes(filter.toLowerCase()) ||
+            user.email.toLowerCase().includes(filter.toLowerCase()) ||
+            user.address.toLowerCase().includes(filter.toLowerCase()) ||
+            new Date(user.registered).toLocaleDateString('fr-FR').toLowerCase().includes(filter.toLowerCase()) ||
+            user.isActive.toString().includes(filter.toLowerCase())
+        ) {
+            return user;
+        }
+    }).map(key => {
+            return (
+                <tr key={ key.id }>
+                    <td>{ key.profile.firstName }</td>
+                    <td>{ key.profile.lastName }</td>
+                    <td>{ key.email }</td>
+                    <td>{ key.address }</td>
+                    <td>{ new Date(key.registered).toLocaleDateString('fr-FR') }</td>
+                    <td>{ key.isActive.toString() }</td>
+                </tr> 
+            )
+        })
+    ;
+
     return (
-        <Table striped bordered hover variant="dark">
-            <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>E-mail</th>
-                    <th>Address</th>
-                    <th>Registered</th>
-                    <th>Status</th>
+        <div className="container">
+            <div className="container">
+                <Form.Control value={ filter } onChange={ e => setFilter( e.target.value) } size="xl" type="text" placeholder="Search for an User here..." />
+            </div>
 
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    Object.keys(users).map(key => {
-                        return (
-                            <tr key={key}>
-                                <td>{ users[key].profile.firstName }</td>
-                                <td>{ users[key].profile.lastName }</td>
-                                <td>{ users[key].email }</td>
-                                <td>{ users[key].address }</td>
-                                <td>{ users[key].registered }</td>
-                                <td>{ users[key].isActive ? 'true': 'false' }</td>
-                            </tr> 
-                        )
-                    })
-                }
-            </tbody>
-        </Table>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>E-mail</th>
+                        <th>Address</th>
+                        <th>Registered</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { items }
+                </tbody>
+            </Table>
+        </div>
     );
-
 }
 
 export default UsersTable;
